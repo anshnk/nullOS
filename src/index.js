0762a3ca268b370b7e322066ba9196fcc94d6ebe
My Name <my-name@chromium.org>
2024-01-05 16:28:10 -0600

    remove gitignore file, Fixed certain page elements not loading using Ultraviolet. Changed the functionality of the wallpaper changer.

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8081;
+if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
   const address = server.address();
